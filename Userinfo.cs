28b1377d1b61c86ef7a4ba5c8d69cc0d1416eb7f
Stratege <Stratege@users.noreply.github.com>
2020-03-01 15:56:40 +0100

    added !userinfo command which gives misc info about people on the server

---
+++ Userinfo.cs
@@ -0,0 +1,38 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace borkbot
+{
+    class Userinfo : CommandHandler
+    {
+        public Userinfo(VirtualServer _server) : base(_server)
+        {
+        }
+
+        public override List<Command> getCommands()
+        {
+            var x = new List<Command>();
+            x.Add(Command.AdminCommand(server, "userinfo", userinfo, new HelpMsgStrings("Provides all manners of interesting information about a user", "userinfo <usermention>")));
+            return x;
+        }
+
+        private void userinfo(ServerMessage e, string m)
+        {
+            var user = Funcs.GetUserByMentionOrName(e.Server.Users, m);
+            if(user != null) { 
+                var eb = new Discord.EmbedBuilder().WithAuthor("The Overbork", server.DC.CurrentUser.GetAvatarUrl()).WithCurrentTimestamp();
+                eb.WithTitle(user.Nickname + "(" + user.Username + ")").WithDescription(user.Mention);
+                eb.AddInlineField("Status", user.Status).AddInlineField("Joined", user.JoinedAt).AddInlineField("Hierarchy Position", user.Hierarchy).AddInlineField("Registered", user.CreatedAt);
+                var embed = eb.Build();
+                server.safeSendEmbed(e.Channel, embed);
+            }else
+            {
+                server.safeSendMessage(e.Channel,"Could not find user: " + m);
+            }
+        }
+    }
+}
