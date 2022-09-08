bc8ef9d2a03b98524d3d6a0f6802d4dfa8707631
Stratege <Stratege@users.noreply.github.com>
2022-09-08 14:18:07 +0200

    cleaned up program.cs and split out the helper functions

---
+++ HelperFunctions.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * Collection of helper functions for serializing/deserializing as well as other small functions
+ * 
+ * */
+
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace borkbot
+{
+    public class DictSerHelper<T, K>
+    {
+        public T car;
+        public K cdr;
+        public DictSerHelper(T _car, K _cdr) { car = _car; cdr = _cdr; }
+        public DictSerHelper() { }
+    }
+
+    public static class Funcs
+    {
+
+
+        public static T XMLSetup<T>(String filepath)
+        {
+            using (var filestream = File.Open(filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                if (filestream.Length > 0)
+                    return (T)new XmlSerializer(typeof(T)).Deserialize(filestream);
+                else
+                    return default(T);
+            }
+        }
+
+        public static Dictionary<T, K> XMlDictSetup<T, K>(String filepath)
+        {
+            var xs = XMLSetup<List<DictSerHelper<T, K>>>(filepath);
+            if (xs == null)
+                return new Dictionary<T, K>();
+            var dict = new Dictionary<T, K>(xs.Count);
+            foreach (var x in xs)
+            {
+                dict.Add(x.car, x.cdr);
+            }
+            return dict;
+        }
+
+        public static void XMLSerialization<T>(String filepath, T obj)
+        {
+            using (var filestream = File.Open(filepath, FileMode.Create, FileAccess.Write))
+            {
+                new XmlSerializer(typeof(T)).Serialize(filestream, obj);
+            }
+        }
+
+        public static void XMlDictSerialization<T, K>(String filepath, Dictionary<T, K> dict)
+        {
+            var xs = new List<DictSerHelper<T, K>>(dict.Count);
+            foreach (var x in dict)
+            {
+                xs.Add(new DictSerHelper<T, K>(x.Key, x.Value));
+            }
+            XMLSerialization<List<DictSerHelper<T, K>>>(filepath, xs);
+        }
+
+
+        public static bool validateMentionTarget(ServerMessage e, String m)
+        {
+            var users = e.msg.MentionedUsers.Where(x => !x.IsBot).Select(x => Tuple.Create(x.Mention, x.Id, x)).ToList();
+            //turning this lax... let's hope it works
+            //return (users.Count != 0 && m == users[0].Item1);
+            return (users.Count != 0);
+        }
+
+        public static List<string> splitKeepDelimiters(string input, string[] delimiters)
+        {
+            int[] nextPosition = delimiters.Select(d => input.IndexOf(d)).ToArray();
+            List<string> result = new List<string>();
+            int pos = 0;
+            while (true)
+            {
+                int firstPos = int.MaxValue;
+                string delimiter = null;
+                for (int i = 0; i < nextPosition.Length; i++)
+                {
+                    if (nextPosition[i] != -1 && nextPosition[i] < firstPos)
+                    {
+                        firstPos = nextPosition[i];
+                        delimiter = delimiters[i];
+                    }
+                }
+                if (firstPos != int.MaxValue)
+                {
+                    result.Add(input.Substring(pos, firstPos - pos));
+                    result.Add(delimiter);
+                    pos = firstPos + delimiter.Length;
+                    for (int i = 0; i < nextPosition.Length; i++)
+                    {
+                        if (nextPosition[i] != -1 && nextPosition[i] < pos)
+                        {
+                            nextPosition[i] = input.IndexOf(delimiters[i], pos);
+                        }
+                    }
+                }
+                else
+                {
+                    result.Add(input.Substring(pos));
+                    break;
+                }
+            }
+            return result;
+        }
+
+
+        //taken straight from: http://stackoverflow.com/a/9461311
+        public static R WithTimeout<R>(Func<R> proc, int millisecondsDuration)
+        {
+            var reset = new System.Threading.AutoResetEvent(false);
+            var r = default(R);
+            Exception ex = null;
+
+            var t = new System.Threading.Thread(() =>
+            {
+                try
+                {
+                    r = proc();
+                }
+                catch (Exception e)
+                {
+                    ex = e;
+                }
+                reset.Set();
+            });
+
+            t.Start();
+
+            // not sure if this is really needed in general
+            while (t.ThreadState != System.Threading.ThreadState.Running)
+            {
+                System.Threading.Thread.Sleep(0);
+            }
+
+            if (!reset.WaitOne(millisecondsDuration))
+            {
+                t.Abort();
+                throw new TimeoutException();
+            }
+
+            if (ex != null)
+            {
+                throw ex;
+            }
+
+            return r;
+        }
+
+        static public SocketGuildUser GetUserByMentionOrName(IEnumerable<SocketGuildUser> users, String str)
+        {
+            String mentionString;
+            if (str[0] == '<' && str[1] == '@' && str[2] != '!')
+            {
+                mentionString = "<@!" + str.Substring(2);
+            }
+            else
+            {
+                mentionString = str;
+            }
+            return users.FirstOrDefault(x => x.Mention == mentionString /*|| x.NicknameMention == split[0]*/ || x.Username == str || x.Nickname == str);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Setting up interface with the framework and connecting the callbacks to the appropriate Virtual Server.
+ * Also handles new server joining.
+ * 
+ * */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +33,6 @@ namespace borkbot
             servers = new Dictionary<ulong, VirtualServer>();
             Console.WriteLine("Booting up");
             DiscordSocketConfig cfgbld = new DiscordSocketConfig();
-            //            DiscordConfigBuilder cfgbld = new DiscordConfigBuilder();
             cfgbld.MessageCacheSize = 100000;
             Console.WriteLine("WebSocketProvider: " + cfgbld.WebSocketProvider);
             cfgbld.WebSocketProvider = Discord.Net.Providers.WS4Net.WS4NetProvider.Instance;
@@ -94,20 +99,6 @@ namespace borkbot
                  servers[guild.Id].userLeft(guild,user);
              });
 
-            //todo: fix
-/*            DC.UserUpdated += async (userOld, userNew) =>
-             await Task.Run(() =>
-             {
-                 if (userNew.GetType() == typeof(SocketGuildUser))
-                 {
-                     servers[((SocketGuildUser)userNew).Guild.Id].userUpdated((SocketGuildUser)userOld, (SocketGuildUser)userNew);
-                 }
-                 else
-                 {
-                     Console.WriteLine("Unhandled User Update received:\n" + userOld + "\nand\n" + userNew);
-                 }
-             });*/
-
             DC.RoleUpdated += async (oldRole, newRole) =>
             await Task.Run(() => {
                 servers[oldRole.Guild.Id].roleUpdated(oldRole, newRole);
@@ -144,12 +135,6 @@ namespace borkbot
                 Console.WriteLine(DateTime.Now + " - Connected to: " + sg.Name);
             });
 
-            /*            this memleaks everywhere, well really it's more the adding bit but whatever, when dormant it should be harmless
-                        DC.ServerUnavailable += (o, e) =>
-                        {
-                            servers.Remove(e.Server);
-                        };
-                        */
             DC.Connected += () =>
             {
                 Console.WriteLine("connected for real");
@@ -160,6 +145,7 @@ namespace borkbot
                 Console.WriteLine("Log Message: " + msg);
                 Console.WriteLine("test: " + msg.Message);
                 //this had to be disabled because discord.net's deserialization code is the biggest jank I have ever seen.
+                //TODO: find a way to recover from log messages when the framework got stuck and the bot needs a full reboot
 /*                if (msg.Exception != null || msg.Message == null || msg.Message.ToLower() == "failed to resume previous session")
                 {
                     //we just, you know, quit
@@ -217,184 +203,5 @@ namespace borkbot
             await Task.Delay(-1);
         }
     }
-    /*
-    public class EmojiAddRequest : Discord.API.IRestRequest
-    {
-        string Discord.API.IRestRequest.Method => "PUT";
-        string Discord.API.IRestRequest.Endpoint => $"channels/{ChannelId}/messages/{MessageId}/reactions/{Emoji}/@me";
-        object Discord.API.IRestRequest.Payload => null;
 
-        public ulong ChannelId { get; set; }
-        public ulong MessageId { get; set; }
-        public string Emoji { get; set; }
-
-        public EmojiAddRequest(ulong channelId, ulong messageId, string emoji)
-        {
-            ChannelId = channelId;
-            MessageId = messageId;
-            Emoji = emoji;
-            Console.WriteLine(((Discord.API.IRestRequest)this).Endpoint);
-        }
-    }
-    */
-    public class DictSerHelper<T, K>
-    {
-        public T car;
-        public K cdr;
-        public DictSerHelper(T _car, K _cdr) { car = _car; cdr = _cdr; }
-        public DictSerHelper() { }
-    }
-
-    public static class Funcs
-    {
-
-
-        public static T XMLSetup<T>(String filepath)
-        {
-            using (var filestream = File.Open(filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                if (filestream.Length > 0)
-                    return (T)new XmlSerializer(typeof(T)).Deserialize(filestream);
-                else
-                    return default(T);
-            }
-        }
-
-        public static Dictionary<T, K> XMlDictSetup<T, K>(String filepath)
-        {
-            var xs = XMLSetup<List<DictSerHelper<T, K>>>(filepath);
-            if (xs == null)
-                return new Dictionary<T, K>();
-            var dict = new Dictionary<T, K>(xs.Count);
-            foreach (var x in xs)
-            {
-                dict.Add(x.car, x.cdr);
-            }
-            return dict;
-        }
-
-        public static void XMLSerialization<T>(String filepath, T obj)
-        {
-            using (var filestream = File.Open(filepath, FileMode.Create, FileAccess.Write))
-            {
-                new XmlSerializer(typeof(T)).Serialize(filestream, obj);
-            }
-        }
-
-        public static void XMlDictSerialization<T, K>(String filepath, Dictionary<T, K> dict)
-        {
-            var xs = new List<DictSerHelper<T, K>>(dict.Count);
-            foreach (var x in dict)
-            {
-                xs.Add(new DictSerHelper<T, K>(x.Key, x.Value));
-            }
-            XMLSerialization<List<DictSerHelper<T, K>>>(filepath, xs);
-        }
-
-
-        public static bool validateMentionTarget(ServerMessage e, String m)
-        {
-            var users = e.msg.MentionedUsers.Where(x => !x.IsBot).Select(x => Tuple.Create(x.Mention, x.Id, x)).ToList();
-            //turning this lax... let's hope it works
-            //return (users.Count != 0 && m == users[0].Item1);
-            return (users.Count != 0);
-        }
-
-        public static List<string> splitKeepDelimiters(string input, string[] delimiters)
-        {
-            int[] nextPosition = delimiters.Select(d => input.IndexOf(d)).ToArray();
-            List<string> result = new List<string>();
-            int pos = 0;
-            while (true)
-            {
-                int firstPos = int.MaxValue;
-                string delimiter = null;
-                for (int i = 0; i < nextPosition.Length; i++)
-                {
-                    if (nextPosition[i] != -1 && nextPosition[i] < firstPos)
-                    {
-                        firstPos = nextPosition[i];
-                        delimiter = delimiters[i];
-                    }
-                }
-                if (firstPos != int.MaxValue)
-                {
-                    result.Add(input.Substring(pos, firstPos - pos));
-                    result.Add(delimiter);
-                    pos = firstPos + delimiter.Length;
-                    for (int i = 0; i < nextPosition.Length; i++)
-                    {
-                        if (nextPosition[i] != -1 && nextPosition[i] < pos)
-                        {
-                            nextPosition[i] = input.IndexOf(delimiters[i], pos);
-                        }
-                    }
-                }
-                else
-                {
-                    result.Add(input.Substring(pos));
-                    break;
-                }
-            }
-            return result;
-        }
-
-
-        //taken straight from: http://stackoverflow.com/a/9461311
-        public static R WithTimeout<R>(Func<R> proc, int millisecondsDuration)
-        {
-            var reset = new System.Threading.AutoResetEvent(false);
-            var r = default(R);
-            Exception ex = null;
-
-            var t = new System.Threading.Thread(() =>
-            {
-                try
-                {
-                    r = proc();
-                }
-                catch (Exception e)
-                {
-                    ex = e;
-                }
-                reset.Set();
-            });
-
-            t.Start();
-
-            // not sure if this is really needed in general
-            while (t.ThreadState != System.Threading.ThreadState.Running)
-            {
-                System.Threading.Thread.Sleep(0);
-            }
-
-            if (!reset.WaitOne(millisecondsDuration))
-            {
-                t.Abort();
-                throw new TimeoutException();
-            }
-
-            if (ex != null)
-            {
-                throw ex;
-            }
-
-            return r;
-        }
-
-        static public SocketGuildUser GetUserByMentionOrName(IEnumerable<SocketGuildUser> users, String str)
-        {
-            String mentionString;
-            if (str[0] == '<' && str[1] == '@' && str[2] != '!')
-            {
-                mentionString = "<@!" + str.Substring(2);
-            }
-            else
-            {
-                mentionString = str;
-            }
-            return users.FirstOrDefault(x => x.Mention == mentionString /*|| x.NicknameMention == split[0]*/ || x.Username == str || x.Nickname == str);
-        }
-
-    }
 }
